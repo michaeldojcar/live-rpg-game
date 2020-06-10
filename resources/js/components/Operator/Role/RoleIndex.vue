144b5db257c91f5900c6ac65db36317e7efc6665
Barbora Kuchaříková <kucharikova@softici.cz>
2020-06-10 14:24:26 +0200

    dark themed admin

--- resources/js/components/Operator/Role/RoleIndex.vue
+++ resources/js/components/Operator/Role/RoleIndex.vue
@@ -4,19 +4,20 @@
 
         <h4>Postavy</h4>
 
-        <table class="w-50 table table-bordered">
-            <tr>
-                <th>Jméno</th>
-                <th>Reálné jméno</th>
-                <th>Úkoly</th>
-            </tr>
-            <tr v-for="role in roles" :key="role.id">
-                <td>
-                    <router-link to="#">{{role.name}}</router-link>
-                </td>
-                <td>{{role.real_name}}</td>
-                <td>{{role.quests_count}}</td>
-            </tr>
+        <table class="table w-50 mt-4" style="border-radius: 0.25rem!important;">
+            <div style="padding: 10px 15px">
+                <tr class="w-100">
+                    <th class="w-50" scope="col" style="border: none">Jméno</th>
+                    <th class="w-50" style="border: none">Reálné jméno</th>
+                    <th class="w-25" scope="col" style="border: none">Úkoly</th>
+                <tr v-for="role in roles" :key="role.id">
+                    <td>
+                        <router-link to="#">{{role.name}}</router-link>
+                    </td>
+                    <td>{{role.real_name}}</td>
+                    <td>{{role.quests_count}}</td>
+                </tr>
+            </div>
         </table>
     </div>
 </template>
