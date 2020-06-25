d6bf345b84e28a8a3b4786ab22a655458faeab8b
Michael Dojčár <michaeldojcar@gmail.com>
2020-06-25 16:11:29 +0200

    requests

--- app/Http/Requests/QuestGroupStoreRequest.php
+++ app/Http/Requests/QuestGroupStoreRequest.php
@@ -13,7 +13,7 @@ class QuestGroupStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class QuestGroupStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'   => 'required|string',
+            'active' => 'required|bool'
         ];
     }
 }
--- app/Http/Requests/QuestGroupUpdateRequest.php
+++ app/Http/Requests/QuestGroupUpdateRequest.php
@@ -24,7 +24,8 @@ class QuestGroupUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'   => 'required|string',
+            'active' => 'required|bool'
         ];
     }
 }
